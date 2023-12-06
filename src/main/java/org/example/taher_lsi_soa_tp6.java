4d89aab3ff37605a05bcd28c9964e6dbd657fb69
taherghrab <taher1.ghrab@gmail.com>
2023-12-06 23:29:24 +0100

    first at

--- src/main/java/org/example/taher_lsi_soa_tp6.java
+++ src/main/java/org/example/taher_lsi_soa_tp6.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 
 import java.util.Date;
 
